34feed40d395a460c34280b26f8078bdc3b0ed7f
Satyam Vats <satyamvats2004@gmail.com>
2026-02-23 22:41:14 +0530

    Imported relationship for user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Integer, String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base import Base
 
